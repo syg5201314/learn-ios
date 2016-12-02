00f43ce88c3059ca4abcea77e76c4c38d4d1f332
Sunning <shanyg@live.com>
2016-12-02 13:33:28 +0800

    Initial Commit

---
+++ learn-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learn-IOS
+//
+//  Created by Sunning on 2016/12/2.
+//  Copyright © 2016年 Sunning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learn-IOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  learn-IOS
+//
+//  Created by Sunning on 2016/12/2.
+//  Copyright © 2016年 Sunning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
